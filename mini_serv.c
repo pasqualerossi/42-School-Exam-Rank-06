c7e268152839e55a5851ca29b1d10df624032aba
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-09-30 14:07:03 +0930

    Add files via upload

--- mini_serv.c
+++ mini_serv.c
@@ -1,96 +1,79 @@
-#include <errno.h>
 #include <string.h>
 #include <unistd.h>
-#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
 
-int extract_message(char **buffer, char **message)
+typedef int bool;
+
+bool	check_arguments(int argc)
 {
-	char	*new_buffer;
-	int		i = 0;
-	
-	*message = 0;
-	if (*buffer == 0)
-		return (0);
-	while ((*buffer)[i])
+	return (argc > 1);
+}
+
+void	putstr_fd(char *str, int fd)
+{
+	write(fd, str, strlen(str));
+}
+
+void	arguments_error(void)
+{
+	putstr_fd("Wrong number of arguments\n", 2);
+	exit(1);
+}
+
+void	fatal_error(void)
+{
+	putstr_fd("Fatal error\n", 2);
+	exit(1);
+}
+
+int get_port(char *arg)
+{
+	int	port = atoi(arg);
+	return (port);
+}
+
+int	main(int argc, char **argv)
+{
+	int						port;
+	int						server_fd;
+	int						client_fd;
+	char					buf[1024];
+	struct sockaddr_in		addr;
+	socklen_t 				addr_len;
+	fd_set					fds;
+
+	if (!check_arguments(argc))
+		arguments_error();
+	if ((port = get_port(argv[1])) == -1)
+		fatal_error();
+	printf("port = %d\n", port);
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr_len = sizeof(addr);
+	if (bind(server_fd, (struct sockaddr *)&addr, addr_len)== -1)
+		fatal_error();
+	if (listen(server_fd, 0) == -1)
+		fatal_error();
+	while (1)
 	{
-		if ((*buffer)[i] == '\n')
+		if ((client_fd = accept(server_fd, (struct sockaddr *)&addr, &addr_len)) == -1)
+			fatal_error();
+		FD_ZERO(&fds);
+		FD_SET(client_fd, &fds);
+		select(client_fd + 1, &fds, NULL, NULL, NULL);
+		if (FD_ISSET(client_fd, &fds))
 		{
-			new_buffer = calloc(1, sizeof(*new_buffer) * (strlen(*buffer + i + 1) + 1));
-			if (new_buffer == 0)
-				return (-1);
-			strcpy(new_buffer, *buffer + i + 1);
-			*message = *buffer;
-			(*message)[i + 1] = 0;
-			*buffer = new_buffer;
-			return (1);
+			recv(client_fd, buf, 1024, 0);
+			printf("Recv: %s\n", buf);
 		}
-		i++;
 	}
 	return (0);
-}
-
-char *string_join(char *buffer, char *add)
-{
-	char	*new_buffer;
-	int		length;
-
-	if (buffer == 0)
-		length = 0;
-	else
-		length = strlen(buffer);
-	new_buffer = malloc(sizeof(*new_buffer) * (length + strlen(add) + 1));
-	if (new_buffer == 0)
-		return (0);
-	new_buffer[0] = 0;
-	if (buffer != 0)
-		strcat(new_buffer, buffer);
-	free(buffer);
-	strcat(new_buffer, add);
-	return (new_buffer);
-}
-
-
-int main() 
-{
-	int sockfd, connfd, len;
-	struct sockaddr_in servaddr, cli; 
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) 
-	{ 
-		printf("socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
-
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(2130706433);
-	servaddr.sin_port = htons(8081); 
-  
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) 
-	{ 
-		printf("socket bind failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully binded..\n");
-	
-	if (listen(sockfd, 10) != 0) 
-	{
-		printf("cannot listen\n"); 
-		exit(0); 
-	}
-	len = sizeof(cli);
-	connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-	if (connfd < 0) 
-	{ 
-        printf("server acccept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server acccept the client...\n");
 }
